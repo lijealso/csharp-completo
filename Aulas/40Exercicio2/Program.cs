e528abedf87f24aa4a609168d9777f1bec2c013d
lijealso <lijealso@gmail.com>
2021-11-16 22:14:47 +0000

    Aula 40: exercício 2

---
+++ Aulas/40Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario1 = new Funcionario();
+            Funcionario funcionario2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            funcionario1.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario1.salario = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            funcionario2.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Salário médio = " + ((funcionario1.salario + funcionario2.salario) / 2.0).ToString("F2"));
+
+            Console.ReadKey();
+        }
+    }
+}
