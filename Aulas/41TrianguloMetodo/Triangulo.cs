da14c30d1adf1761bc32b9723f90284361967c84
lijealso <lijealso@gmail.com>
2021-11-16 22:30:39 +0000

    rename

---
+++ Aulas/41TrianguloMetodo/Triangulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _41TrianguloMetodo
+{
+    internal class Triangulo
+    {
+        public double A, B, C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+
+            double area = Math.Sqrt( p * (p - A) * (p - B) * (p- C) );
+
+            return area;
+        }
+    }
+}
