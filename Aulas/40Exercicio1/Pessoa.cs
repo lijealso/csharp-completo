33310babc0776f6468e01f9e1dbcb77874ce78c2
lijealso <lijealso@gmail.com>
2021-11-16 22:03:41 +0000

    Aula 40: exercício 1

---
+++ Aulas/40Exercicio1/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40Exercicio1
+{
+    public class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+
+    }
+}
---
+++ Aulas/40Exercicio1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _40Exercicio1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            Pessoa p2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.WriteLine("Nome: ");
+            p1.nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.WriteLine("Nome: ");
+            p2.nome = Console.ReadLine();
+            Console.WriteLine("Idade: ");
+            p2.idade = int.Parse(Console.ReadLine());
+
+            if(p1.idade > p2.idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.nome);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
