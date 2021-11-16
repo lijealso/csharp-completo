7c166855b256abb078deb6b0e9ae47d8cb4d5fa7
lijealso <lijealso@gmail.com>
2021-11-16 20:49:12 +0000

    aula 34: ciclo for

--- Aulas/34For/Program.cs
+++ Aulas/34For/Program.cs
@@ -6,7 +6,19 @@ namespace _34For
     {
         static void Main(string[] args)
         {
-            
+            int soma = 0;
+            Console.WriteLine("Quantos números inteiros? ");
+            int N = int.Parse(Console.ReadLine());
+
+
+            for (int i = 1 ; i <= N ; i++)
+            {
+                Console.WriteLine("Insira o número # " + i);
+                soma = soma + int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Soma = " + soma);
+
         }
     }
 }
