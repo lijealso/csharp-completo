0877ab264a228bf7693a463add03df8a44c95984
lijealso <lijealso@gmail.com>
2021-11-17 21:39:16 +0000

    Aula 71: exercício

---
+++ Aulas/71Exercicio/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _71Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos vão ser alugados: ");
+            int q = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < q  ; i++)
+            {
+                Console.WriteLine("Aluguer #" + (i + 1));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);            
+            }
+
+            for (int i = 0 ; i < vect.Length ; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
