d02086da1f0980bfb4c24c6c072cb71d8fc8219d
lijealso <lijealso@gmail.com>
2021-11-17 16:25:17 +0000

    Aula 69: vetores, parte 1

---
+++ Aulas/69Vetores/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _69Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0.0;
+            Console.WriteLine("Quantos números: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Número " + (i + 1) + ": " );
+                vect[i] = double.Parse(Console.ReadLine());
+                soma = soma + vect[i];
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Posição" + i + " = " + vect[i]);
+            }
+
+            Console.WriteLine("MÉDIA = " + (soma / vect.Length).ToString("F2"));
+
+            Console.ReadKey();
+        }
+    }
+}
