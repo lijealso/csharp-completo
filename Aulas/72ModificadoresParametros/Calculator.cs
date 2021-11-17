9c43dd511289e299011bb371e1687fa76f366aea
lijealso <lijealso@gmail.com>
2021-11-17 22:06:27 +0000

    Aula 72: modificadores de parâmetros params

---
+++ Aulas/72ModificadoresParametros/Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72ModificadoresParametros
+{
+    class Calculator
+    {
+        /*
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public static int Sum(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+        public static int Sum(int n1, int n2, int n3, int n4)
+        {
+            return n1 + n2 + n3 + n4;
+        }
+        */
+
+        public static int Sum(params int[] numbers) // <-- adiciona-se "params"
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length ; i++)
+            {
+                sum = sum + numbers[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Aulas/72ModificadoresParametros/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72ModificadoresParametros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {         
+            // int s1 = Calculator.Sum(2, 3);
+
+            // int s1 = Calculator.Sum(new int[] {2, 3});
+
+            // int s2 = Calculator.Sum(3, 4, 5);
+
+            // int s2 = Calculator.Sum(new int[] {4, 5, 6});
+
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(3, 4, 6);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            
+            Console.ReadKey();
+        }
+    }
+}
