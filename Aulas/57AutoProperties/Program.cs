4d4f9af980a3a071515511d8849fb33dd66fd189
lijealso <lijealso@gmail.com>
2021-11-17 13:02:23 +0000

    Aula 57: auto properties

---
+++ Aulas/57AutoProperties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _57AutoProperties
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.Nome = "T";
+            Console.WriteLine(p.Nome);
+
+            p.Preco = 555.0;
+            Console.WriteLine(p.Preco);
+
+            Console.ReadKey();  
+        }
+    }
+}
