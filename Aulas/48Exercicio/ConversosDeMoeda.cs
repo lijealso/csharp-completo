4797c37861078e0be6f4f4cd38d8ab121e12b48e
lijealso <lijealso@gmail.com>
2021-11-17 02:12:29 +0000

    Aula 48: exercício

---
+++ Aulas/48Exercicio/ConversosDeMoeda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48Exercicio
+{
+    public class ConversosDeMoeda
+    {
+        public static double IOF = 0.06;
+
+        public static double Converte(double cotacao, double quantidade)
+            {
+            double resultado;
+
+            resultado = quantidade * cotacao;
+
+            return resultado + resultado * IOF;
+            }
+    }
+}
---
+++ Aulas/48Exercicio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _48Exercicio
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ConversosDeMoeda conversor = new ConversosDeMoeda();
+
+            Console.Write("Qual a cotação do dólar: ");
+            double cotacao = double.Parse(Console.ReadLine());
+
+            Console.Write("Quantos dólares vai comprar: ");
+            double quantidade = double.Parse(Console.ReadLine());
+
+            double resultado = ConversosDeMoeda.Converte(cotacao, quantidade);
+
+            Console.WriteLine("Valor a ser pago: " + resultado.ToString("F2"));
+
+            Console.ReadLine();
+        }
+    }
+}
