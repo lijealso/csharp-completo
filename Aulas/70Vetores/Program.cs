d2cde10e31927332a305072d03f3bdc724c61405
lijealso <lijealso@gmail.com>
2021-11-17 21:05:09 +0000

    Aula 70: vetores, parte 2

---
+++ Aulas/70Vetores/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _70Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+
+            for(int i = 0 ; i < n ; i++)
+            {
+                sum += vect[i].Price;
+            }
+
+            Console.WriteLine("AVERAGE PRICE = " + (sum/vect.Length).ToString("F2"));
+
+            Console.ReadLine();
+        }
+
+    }
+}
