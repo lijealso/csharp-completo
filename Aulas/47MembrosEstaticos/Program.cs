b3b7b0414436e14f90f2a0fbdf60e4f26a1e1558
lijealso <lijealso@gmail.com>
2021-11-17 01:46:07 +0000

    Aula 47: membros estáticos

---
+++ Aulas/47MembrosEstaticos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _47MembrosEstaticos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            Console.WriteLine("Qual o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2"));
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2"));
+
+            Console.ReadKey();
+        }
+    }
+}
