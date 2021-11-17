ffd5feafb4c917367d3a45500356bd1ee7c84a4e
lijealso <lijealso@gmail.com>
2021-11-17 16:03:19 +0000

    Aula 68: nullables

---
+++ Aulas/68Nullable/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _68Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+
+            if(x.HasValue)  
+                Console.WriteLine(x.Value);
+            else 
+                Console.WriteLine("X is null");
+
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+
+            double a = x ?? 5.0;
+            double b = y ?? 5.0;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
