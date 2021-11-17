08b1b9c339dad69ada4988eda3456cc4a1e56850
lijealso <lijealso@gmail.com>
2021-11-17 00:08:48 +0000

    Aula 45: exercicio 1

---
+++ Aulas/45Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45Exercicio1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Insira a largura e altura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine());
+            retangulo.Altura = double.Parse(Console.ReadLine());
+            Console.WriteLine(retangulo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Aulas/45Exercicio1/Retangulo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45Exercicio1
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (2.0 * Largura) + (2.0 * Altura);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Largura,2) + Math.Pow(Altura,2));
+        }
+
+        public override string ToString()
+        {
+            return "AREA = "
+                + Area().ToString("F2")
+                + "\nPERÍMETRO: "
+                + Perimetro().ToString("F2")
+                + "\nDIAGONAL: "
+                + Diagonal().ToString("F2");
+        }
+    }
+}
