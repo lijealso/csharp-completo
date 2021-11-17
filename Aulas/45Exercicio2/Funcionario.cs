bb68d251605d24b6e3b6f071dd4f8fc4a7e49b29
lijealso <lijealso@gmail.com>
2021-11-17 00:32:43 +0000

    Aula 45: exercicio 2

---
+++ Aulas/45Exercicio2/Funcionario.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45Exercicio2
+{
+    public class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public Funcionario()
+        {
+        }
+
+        public Funcionario(string nome, double salarioBruto, double imposto)
+        {
+            Nome = nome;
+            SalarioBruto = salarioBruto;
+            Imposto = imposto;
+        }
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                    + ", $ "
+                    + SalarioLiquido().ToString("F2");
+        }
+    }
+}
---
+++ Aulas/45Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Funcionario: " + funcionario);
+
+            Console.Write("Digite a percentagem para aumenta o salário: ");
+            funcionario.AumentarSalario(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados atualizados: " + funcionario);
+
+            Console.ReadLine();
+
+        }
+    }
+}
