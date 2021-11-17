be82712da1d011b4f3429bf0454d379557efcf54
lijealso <lijealso@gmail.com>
2021-11-17 01:02:32 +0000

    Aula 45: exercício 3

---
+++ Aulas/45Exercicio3/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45Exercicio3
+{
+    public class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+        public double CalculaNotaFinal()
+        {
+            double resultado = Nota1 + Nota2 + Nota3;
+            return resultado;
+        }
+
+        public string AferirPassagem()
+        {
+            double resultado = Nota1 + Nota2 + Nota3;
+
+            if (resultado > 60.0)
+            {
+                return "APROVADO";
+            }
+            else
+            {
+                return "REPROVADO\n"
+                    + "FALTARAM "
+                    + (60.0 - resultado).ToString("F2")
+                    + " PONTOS";
+            }
+        }
+    }
+}
---
+++ Aulas/45Exercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _45Exercicio3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Insira as 3 notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+            Console.Write("Nota final = " + aluno.CalculaNotaFinal().ToString("F2") + "\n");
+            Console.WriteLine(aluno.AferirPassagem());
+
+            Console.ReadLine();
+        }
+    }
+}
