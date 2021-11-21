26642dcfc4099268cd3d1b820f32c43004918f92
lijealso <lijealso@gmail.com>
2021-11-21 20:38:15 +0000

    Aula 126: herança

---
+++ Aulas/126Heranca/application/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using _126Heranca.entities;
+
+BusinessAccount account = new BusinessAccount(8010, "Bob Brown, 100.0, 500.0");
+
+Console.WriteLine(account.Balance); // get
+
+// account.Balance = 200.0; // set não acessível / atributo não acessível
+
+
+
