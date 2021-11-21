481e8a04ce2a12ce854b69c0fe63248f1cf8d752
lijealso <lijealso@gmail.com>
2021-11-21 23:31:31 +0000

    Aula 131: exercicio, inheritance

---
+++ Aulas/131Exercicio/entities/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131Exercicio.entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHOur { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name, int hours, double valuePerHOur)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHOur = valuePerHOur;
+        }
+
+        public virtual double Payment()     
+        {
+            return Hours * ValuePerHOur;
+        }
+    }
+}
---
+++ Aulas/131Exercicio/entities/OutsourcedEmployee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _131Exercicio.entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AddicionalCharge { get; set; }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHOur, double additionalCharge)
+           : base (name, hours, valuePerHOur)
+        {
+            AddicionalCharge = additionalCharge;
+        }
+        public override double Payment()
+        {
+            return base.Payment() + AddicionalCharge * 1.1;
+
+        }
+    }
+}
