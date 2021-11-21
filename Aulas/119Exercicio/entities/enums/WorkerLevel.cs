9306832ec8f96dbd18fc5b61680f4c3498bc07c5
lijealso <lijealso@gmail.com>
2021-11-21 19:08:08 +0000

    Aula 119: exercicio, composição

---
+++ Aulas/119Exercicio/entities/enums/WorkerLevel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _119Exercicio.entities.enums
+{
+    enum WorkerLevel : int
+    {
+        Junior,
+        MidLevel,
+        Senior
+    }
+}
